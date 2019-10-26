6ff69c740445497ed66fd94d28ae0f3818a6e034
최철환 <hwanyhee@naver.com>
2019-10-26 17:16:56 +0900

    Counter 컴포넌트의 증가 로직 - 파라메터 전달

--- src/App.js
+++ src/App.js
@@ -4,40 +4,40 @@ import {Header} from './components/Header';
 import {Player} from "./components/Player";
 
 class App extends React.Component {
-  state = {
-    players: [
-      {name: 'LDK', score: 1, id: 1},
-      {name: 'HONG', score: 2, id: 2},
-      {name: 'KIM', score: 3, id: 3},
-      {name: 'PARK', score: 4, id: 4},
-    ]
-  }
-  render() {
-    return (
-      <div className="scoreboard">
-        <Header title="My Scoreboard" totalPlayers={11} />
+	state = {
+		players: [
+			{name: 'LDK', score: 1, id: 1},
+			{name: 'HONG', score: 2, id: 2},
+			{name: 'KIM', score: 3, id: 3},
+			{name: 'PARK', score: 4, id: 4},
+		]
+	}
+	render() {
+		return (
+			<div className="scoreboard">
+				<Header title="My Scoreboard" totalPlayers={11} />
 
-        {/*Player's List*/}
-        {
-          this.state.players.map(player =>
-            <Player name={player.name} score={player.score} id={player.id} key={player.id}
-                    removePlayer={this.handleRemovePlayer.bind(this)}
-                    changeScore={this.handleChangeScore} />)
-        }
-      </div>
-    );
-  }
-  handleRemovePlayer(id) {
-    // 삭제 로직
-    console.log('handleRemovePlayer: ', id);
-    this.setState(prevState => {
-      const players = prevState.players.filter(player => player.id !== id);
-      return {players};
-    })
-  }
-  handleChangeScore() {
-    console.log('handleChangeScore');
-  }
+				{/*Player's List*/}
+				{
+					this.state.players.map(player =>
+						<Player name={player.name} score={player.score} id={player.id} key={player.id}
+										removePlayer={this.handleRemovePlayer.bind(this)}
+										changeScore={this.handleChangeScore} />)
+				}
+			</div>
+		);
+	}
+	handleRemovePlayer(id) {
+		// 삭제 로직
+		console.log('handleRemovePlayer: ', id);
+		this.setState(prevState => {
+			const players = prevState.players.filter(player => player.id !== id);
+			return {players};
+		})
+	}
+	handleChangeScore(id, delta) {
+		console.log('handleChangeScore:', id, delta);
+	}
 }
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 // function  컴포넌트를 class 컴포넌트로 전환
 export class Counter extends React.Component {
-	incrementScore = () => {
+	incrementScore = (delta) => {
 		console.log('increment: ', this);
 
 		// this.state.score += 1; // => 안된다.
-		this.props.changeScore();
+		this.props.changeScore(this.props.id, delta);
 	}
 
 	render() {
 		return (
 			<div className="counter">
-				<button className="counter-action decrement"> - </button>
+				<button className="counter-action decrement" onClick={() => this.incrementScore(-1)}> - </button>
 				<span className="counter-score">{this.props.score}</span>
-				<button className="counter-action increment" onClick={this.incrementScore}> + </button>
+				<button className="counter-action increment" onClick={() => this.incrementScore(1)}> + </button>
 			</div>
 		);
 	}
