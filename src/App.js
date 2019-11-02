30257ead5fdce36f1abf95e26429766b09c662e4
최철환 <hwanyhee@naver.com>
2019-11-02 10:24:46 +0900

    addPlayer -로직 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Header} from './components/Header';
 import {Player} from "./components/Player";
 import {AddPlayerForm} from "./components/AddPlayerForm";
 
+let maxId = 4;
+
 class App extends React.Component {
 	state = {
 		players: [
@@ -60,6 +62,11 @@ class App extends React.Component {
 
 	handleAddPlayer(name) {
 		console.log('handleAddPlayer: ', name);
+		this.setState(prevState => {
+			const players = [...prevState.players];
+			players.push({id: ++maxId, name: name, score: 0});
+			return {players}
+		});
 	}
 }
 
