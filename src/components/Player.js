7c2e9a6e03752418b59c8242ba28c255916bc1ca
최철환 <hwanyhee@naver.com>
2019-11-02 13:23:06 +0900

    optimize - shouldComponent Update 구현하여 부분 렌더링

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export const Player = (props) => (
-	<div className="player">
+export class Player extends React.Component {
+	render() {
+		console.log(this.props.name, ' is rendered');
+		return (
+			<div className="player">
 		<span className="player-name">
-			<button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
-			{props.name}
+			<button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}> x </button>
+			{this.props.name}
 		</span>
-		<Counter score={props.score} id={props.id} changeScore={props.changeScore} />
-	</div>
-);
+				<Counter score={this.props.score} id={this.props.id} changeScore={this.props.changeScore}/>
+			</div>
+		);
+	}
+
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+		// 기존 score와 nextProps의 score 가 다르면 true
+		return this.props.score !== nextProps.score ? true : false;
+	}
+}
