5ebb00fe8efc7cee5e9efc071ff5dd76bf93ba36
최철환 <hwanyhee@naver.com>
2019-11-02 12:10:00 +0900

    Stopwatch - 로직 구현

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,19 +2,36 @@ import React from 'react';
 
 export class Stopwatch extends React.Component {
 	tickRef;
+	state = {
+		isRunning: false,
+		timer: 0
+	}
+
+	handleStopwatch() {
+		this.setState(prevState => ({isRunning: !prevState.isRunning}))
+	}
+	handleReset() {
+		this.setState({timer: 0})
+	}
 
 	render() {
 		return (
 			<div className="stopwatch">
 				<h2>StopWatch</h2>
-				<span className="stopwatch-time">0</span>
-				<button>Start</button>
-				<button>Reset</button>
+				<span className="stopwatch-time">{this.state.timer}</span>
+				<button onClick={this.handleStopwatch.bind(this)}>
+					{this.state.isRunning ? 'Stop' : 'Start'}
+				</button>
+				<button onClick={this.handleReset.bind(this)}>Reset</button>
 			</div>
 		);
 	}
 
-	tick() {}
+	tick = () => {
+		if (this.state.isRunning) {
+			this.setState(prevState => ({timer: prevState.timer + 1}))
+		}
+	}
 
 	// Dom이 렌더링 된 직후에 호출
 	//  jquery, 스크롤바, 등 Dom과 관련된 메서드, 네트워크 호출 초기화
